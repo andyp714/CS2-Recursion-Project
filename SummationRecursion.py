3f6f8304f579677a24b92dcbe4720df6349e78d2
andyp714 <andypauley05@gmail.com>
2021-11-10 08:44:00 -0500

    Create SummationRecursion.py

---
+++ SummationRecursion.py
@@ -0,0 +1,25 @@
+'''
+Created on Nov 10, 2021
+
+@author: apauley24
+'''
+def main():
+    pass
+    n = int(input("WHat number"))
+    print(factorial(n))
+
+
+
+
+def factorial(n):
+    if n == 0:
+        return 0
+    if n == 1:
+        return 1
+    
+    return n + factorial(n-1)
+
+
+
+if __name__ == "__main__":
+    main()
