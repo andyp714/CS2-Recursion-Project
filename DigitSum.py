3be54d75ceed0a34c49ce92b577c2eeca95d7de9
andyp714 <andypauley05@gmail.com>
2021-11-10 09:02:48 -0500

    Create DigitSum.py

---
+++ DigitSum.py
@@ -0,0 +1,22 @@
+'''
+Created on Nov 10, 2021
+
+@author: apauley24
+'''
+def main():
+    pass
+    n = int(input("what number"))
+    print(digitSum(n))
+
+
+
+
+def digitSum(n):
+    if n < 10:
+        return n
+    return (digitSum(int(n/10)) + n % 10)
+
+
+
+if __name__ == "__main__":
+    main()
